b804c3bfad1f3c27192a7144c673459b371a82be
ToanV <49969383+votoan410@users.noreply.github.com>
2020-02-03 10:08:04 -0600

    names updated

--- src/multithreading_assignment/MultiThreading_assignment.java
+++ src/multithreading_assignment/MultiThreading_assignment.java
@@ -1,4 +1,11 @@
 /*
+Names: Alberto, I'munique, Toan
+Class: COSC2353
+Professor: Dr. Melissa Carlton
+Date: 2/3/2020
+
+*/
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
